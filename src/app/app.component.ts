fc5ba8047f095121d449759a74c4608e6f4a3682
azkarmoulana <azkar.moulana@gmail.com>
2020-02-17 18:33:05 +0530

    ssr initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { timingSafeEqual } from 'crypto';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular-ssr';
+export class AppComponent implements OnInit {
+
+  books = [];
+
+  constructor(private http: HttpClient) {
+  }
+
+  ngOnInit() {
+    this.http.get<[]>('http://www.mocky.io/v2/5e4a85b42f00005d0097d2ab').subscribe(
+      (res: any) => {
+        this.books = res;
+      }
+    )
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HttpClientModule  } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    HttpClientModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
